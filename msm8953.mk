6b6d214dc85666793fc93441a358db219f43a75c
mvaisakh <vaisakhmurali@gmail.com>
2020-05-05 01:01:02 +0530

    msm8953-common: edit: Correct the Bluetooth Build Packages

--- msm8953.mk
+++ msm8953.mk
@@ -114,8 +114,8 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl \
-    android.hardware.bluetooth@1.0-service
+    libbluetooth_qti \
+    libbt-logClient.so
 
 # Configstore
 PRODUCT_PACKAGES += \
